18c786fc5bdafd6797a604d6c95ea28c1fbf3da2
PC <adam1532@hotmail.com>
2018-04-12 00:53:05 +0200

    Added egyszamjatek and fogado

---
+++ C#/EgyszamJatek/EgyszamJatek/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EgyszamJatek.Classes;
+using System.IO;
+
+namespace EgyszamJatek {
+    class Program {
+        static void Main( string[] args ) {
+            string[] lines = File.ReadAllLines("egyszamjatek.txt", Encoding.Default);
+            List<Guess> guesses = new List<Guess>();
+            foreach( string line in lines ) {
+                string[] parts = line.Split(' ');
+                string last = parts.Last();
+                List<int> LineGuesses = new List<int>();
+                foreach( string part in parts ) {
+                    if( !part.Equals( last ) ) {
+                        LineGuesses.Add( int.Parse( part ) );
+                    }
+                    else {
+                        guesses.Add( new Guess( part, LineGuesses ) );
+                    }
+                }
+            }
+
+            // 3. feladat
+            Console.WriteLine( "3. feladat: Játékosok száma: {0}", guesses.Count() );
+
+            // 4. feladat
+            Console.WriteLine( "4. feladat: Fordulók száma: {0}", (guesses[0].GetGuesses()).Count() );
+
+            // 5. feladat
+            bool IsNumberOneGuessed = guesses.Any(x => x.GetGuesses()[0] == 1);
+            if( IsNumberOneGuessed ) {
+                Console.WriteLine("5. feladat: Az első fordulóban volt egyes tipp!");
+            }
+            else {
+                Console.WriteLine( "5. feladat: Az első fordulóban nem volt egyes tipp!" );
+            }
+
+            // 6. feladat
+            int max = Int32.MinValue;
+            guesses.ForEach( x => {
+                if( x.GetGuesses().Max() > max ) max = x.GetGuesses().Max();
+            } );
+            Console.WriteLine("6. feladat: A legnagyobb tipp a fordulók során: {0}", max);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Fogado/Fogado/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fogado {
+    class Program {
+        static void Main( string[] args ) {
+            string[] lines = File.ReadAllLines("fogado.csv");
+            lines = lines.Skip( 1 ).ToArray();
+
+            StreamWriter sw = new StreamWriter("nagyletszam.txt");
+            sw.WriteLine( "Nagy létszámúak foglalásai" );
+
+            int SumOfMoreThanThree= 0;
+            int SumOfBigBookings = 0;
+            foreach( string line in lines ) {
+                string[] parts = line.Split(';');
+                int guest = int.Parse(parts[0]);
+                int room = int.Parse(parts[1]);
+                string ArrivalDate = parts[2];
+                string LeavingDate = parts[3];
+                int NumberOfPeople = int.Parse(parts[4]);
+
+                if( NumberOfPeople >= 4 ) {
+                    SumOfMoreThanThree++;
+                }
+                if( NumberOfPeople >= 5 ) {
+                    SumOfBigBookings++;
+                    sw.WriteLine( "Vendég: {0}, szoba: {1}, érkezés: {2}, távozás: {3}, fő: {4}", guest, room, ArrivalDate, LeavingDate, NumberOfPeople );
+                }
+            }
+
+            sw.WriteLine( "Foglalások száma: {0}", SumOfBigBookings );
+
+            sw.Close();
+
+            Console.WriteLine( "1. feladat: {0}-n érkeztek négyen, vagy annál többen.", SumOfMoreThanThree );
+
+            Console.ReadLine();
+        }
+    }
+}
